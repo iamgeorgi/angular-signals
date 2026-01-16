4f78dd46843601a6d22a1c5146f405abfd6af925
iamgeorgi <georgipeetrov@gmail.com>
2026-01-16 12:23:42 +0200

    change detection challenge

--- practice/01. change-detaction/src/app/app.component.ts
+++ practice/01. change-detaction/src/app/app.component.ts
@@ -1,16 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-  // 1. add change detection strategy here
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   counter = 0;
-
-  // 2. inject ChangeDetectorRef here
+  readonly changeDetector = inject(ChangeDetectorRef);
 
   constructor() {
     setInterval(() => {
@@ -18,7 +17,9 @@ export class AppComponent {
       console.log('Counter:', this.counter);
     }, 1000);
 
-    // 3. Add another interval that calls detectChanges() every 5 seconds
+    setInterval(() => {
+      this.changeDetector.detectChanges()
+    }, 5000);
   }
 
 }
