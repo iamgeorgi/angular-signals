20233ca91e43275ab82f789b8286ed0f4ae4c938
iamgeorgi <georgipeetrov@gmail.com>
2026-01-16 17:34:34 +0200

    rxjs change detection

--- practice/02. behavior-subject-state/src/app/app.component.ts
+++ practice/02. behavior-subject-state/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 import { interval } from 'rxjs';
 
 @Component({
@@ -8,19 +8,17 @@ import { interval } from 'rxjs';
   imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  // 1. set change detection strategy to OnPush
 })
-export class AppComponent {
+export class AppComponent implements OnChanges {
   readonly counter$ = interval(1000);
+  someCoolValue = 50;
 
-  // 2. Remove the counter property and the constructor
-  counter = 0;
-
-  constructor() {
-    this.counter$.subscribe((value) => {
-      this.counter = value;
-    });
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Change detection has been invoked!', changes);
   }
 
-  // 3. In the html, bind directly to the counter$ observable using the async pipe
+  doNothing() {
+    // console.log('change has been spotted!');
+    return 42;
+  }
 }
